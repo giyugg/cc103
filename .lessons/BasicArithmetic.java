202c56110f627152aa73bf07599fa26ce9730d0f
GiyuuuGG <personal.kyleacuna@gmail.com>
2025-10-17 18:26:08 +0800

    was on my old repositories and added it here for my future references

---
+++ .lessons/BasicArithmetic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c, d;
+        
+        System.out.print("Enter a: ");
+        a = input.nextInt();
+        System.out.print("Enter b: ");
+        b = input.nextInt();
+        System.out.print("Enter c: ");
+        c = input.nextInt();
+        System.out.print("Enter d: ");
+        d = input.nextInt();
+        input.close(); 
+        
+        int sum = (a + b) * (c + d);
+        
+    System.out.println("(" + a + " + " + b + ") * (" + c + " + " + d + ") = " + sum);
+    }
+}
